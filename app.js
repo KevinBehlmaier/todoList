982f6824265bdd6ef7beab6feba0c955dc4599e2
KevinBehlmaier <kevin.behlmaier@gmail.com>
2018-04-03 16:30:40 -0600

    Adds delete button and styles

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ var btn = document.getElementById('btn')
 var ul = document.getElementById('myList')
 
 
-function updateText() {
-  var newLi = document.createElement("li");
-  newLi.innerHTML = input.value;
-  if (input.value === '') {
-    alert("Your entry is blank.")
-  } else {
-    ul.appendChild(newLi);
-  }
-};
 
+
+// check mark section
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -21,11 +14,51 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-/*document.getElementsByName('li').addEventListener("click", function () {})*/
+// creates delete button
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode(" \u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
+// clicking closed, deletes current item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-/* input.value?
+// man oh man
 
-ul. appendChild(li)
-createElement("LI")
-*/
+function updateText() {
+  var li = document.createElement("li");
+  var inputValue = input.value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (input.value === '') {
+    alert("Your entry is blank.")
+  } else {
+    ul.appendChild(li);
+  }
+inputValue = "";
+// iffy part
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode(" \u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+   close[i].onclick = function() {
+     var div = this.parentElement;
+     div.style.display = "none";
+   }
+ }
+}
